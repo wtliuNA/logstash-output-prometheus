7d3cd4642c40b5609a250a2c438c72ad2a8f1537
root <liuwt1010@gmail.com>
2018-01-16 23:17:08 +0000

    First working version

--- lib/logstash/outputs/prometheus.rb
+++ lib/logstash/outputs/prometheus.rb
@@ -1,22 +1,27 @@
 # encoding: utf-8
 require "logstash/outputs/base"
 require "logstash/namespace"
+require 'json'
 require_relative "alert_manager_client/client"
 
 # An example output that does nothing.
 class LogStash::Outputs::Prometheus < LogStash::Outputs::Base
   config_name "prometheus"
+  @@alert_count = 0
 
   public
   def register
 
   	@am = AlertManagerClient::Client.new(url: 'http://localhost:9093', path: '/api/v1/alerts')
-
   end # def register
 
   public
   def receive(event)
-  	@am.post '{"alertname": "test", "severity": "info"}'
-    return "Event received"
+        #time_start = Time.now.to_datetime.rfc3339
+        #sleep(1)
+        @@alert_count += 1  
+  	# @am.post '[{"labels":{"label1":"test"},"annotations":{"label2":"hello"},"startsAt":"2006-01-02T15:04:05.999999-07:00","endsAt":"2012-11-01T22:08:41+00:00","generatorURL":"fake_url"}]'
+  	json_body = '[{"labels":{"label1":"test4_' + "#@@alert_count" + '"},' + '"annotations":{"label2":"hello"},"generatorURL":"fake_url"}]'
+  	@am.post json_body
   end # def event
 end # class LogStash::Outputs::Prometheus
--- spec/outputs/prometheus_spec.rb
+++ spec/outputs/prometheus_spec.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
-require "logstash/outputs/example"
+require "logstash/outputs/prometheus"
 require "logstash/codecs/plain"
 require "logstash/event"
 
-describe LogStash::Outputs::Example do
+describe LogStash::Outputs::Prometheus do
   let(:sample_event) { LogStash::Event.new }
-  let(:output) { LogStash::Outputs::Example.new }
+  let(:output) { LogStash::Outputs::Prometheus.new }
 
   before do
     output.register
