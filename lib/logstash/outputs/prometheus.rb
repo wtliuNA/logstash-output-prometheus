32b791cd708b14e96c7606cace59331c9ca562e1
Wentao Liu <wentao.l.liu@oracle.com>
2018-01-17 17:28:43 -0800

    refactor

--- lib/logstash/outputs/prometheus.rb
+++ lib/logstash/outputs/prometheus.rb
@@ -30,29 +30,14 @@ class LogStash::Outputs::Prometheus < LogStash::Outputs::Base
   public
   def receive(event)
     
-        #time_start = Time.now.to_datetime.rfc3339
-        #sleep(1)
-        #@@alert_count += 1  
+    #@@alert_count += 1  
   	# @am.post '[{"labels":{"label1":"test"},"annotations":{"label2":"hello"},"startsAt":"2006-01-02T15:04:05.999999-07:00","endsAt":"2012-11-01T22:08:41+00:00","generatorURL":"fake_url"}]'
-  	
     #json_body = '[{"labels":{"label1":"test4_' + "#@@alert_count" + '"},' + '"annotations":{"label2":"hello"},"generatorURL":"fake_url"}]'
   	#@am.post json_body
 
     alert = Hash.new
-    alert['annotations'] = Hash.new
-    alert['labels'] = Hash.new
-    alert['startsAt'] = Time.now.to_datetie.rfc3339
-    alert['endsAt'] = ''
-    alert['generatorURL'] = ''
-    alert['labels']['instance'] = 'syslog'
+    initialize_alert(alert)
 
-    alert['labels']['alertname'] = 'TestAlert'
-    alert['annotations']['summary'] = "apple"
-    alert['labels']['severity'] = 'banana'
-    alert['labels']['project'] = 'fruit'
-    alert['labels']['component'] = 'orange'
-
-=begin
     event.to_hash.each do |key, value|
       case key
       when 'title'
@@ -65,14 +50,27 @@ class LogStash::Outputs::Prometheus < LogStash::Outputs::Base
       else
       end
     end
-=end
 
     alert_json = alert.to_json
-    json_body = '[' + alert_json + ']'
-    @am_client.post json_body
-
-
-
+    alert_json = '[' + alert_json + ']'
+    # p alert_json # print out
+    @am_client.post alert_json
 
   end # def event
+
+  private
+  def initialize_alert(alert)
+    alert['annotations'] = Hash.new
+    alert['labels'] = Hash.new
+    alert['startsAt'] = Time.now.to_datetime.rfc3339
+    #alert['endsAt'] = ''
+    #alert['generatorURL'] = ''
+    alert['labels']['instance'] = 'syslog'
+
+    alert['labels']['alertname'] = 'Default Alert'
+    alert['annotations']['summary'] = "Default Summary"
+    alert['labels']['severity'] = '5'
+    alert['labels']['project'] = 'NM'
+    alert['labels']['component'] = 'Passive Monitoring'
+  end
 end # class LogStash::Outputs::Prometheus
